047a94032078d8e3b91ab0911e72c8a7278d04a3
SoulString <mike@soulstring.io>
2025-08-21 12:22:04 +0400

    fix: temporarily remove missing undocumented django internal

--- django_postgres_extensions/models/fields/related_lookups.py
+++ django_postgres_extensions/models/fields/related_lookups.py
@@ -1,4 +1,4 @@
-from django.db.models.fields.related_lookups import RelatedLookupMixin, MultiColSource, get_normalized_value
+from django.db.models.fields.related_lookups import RelatedLookupMixin, get_normalized_value
 from django.contrib.postgres.fields.array import ArrayContains, ArrayContainedBy, ArrayExact, ArrayOverlap
 
 from django_postgres_extensions.models.lookups import (
@@ -7,7 +7,7 @@ from django_postgres_extensions.models.lookups import (
 class RelatedArrayMixin(RelatedLookupMixin):
     def get_prep_lookup(self):
         self.lookup_name = 'contains'
-        if not isinstance(self.lhs, MultiColSource) and self.rhs_is_direct_value():
+        if self.rhs_is_direct_value():
             self.rhs = [get_normalized_value(value, self.lhs)[0] for value in self.rhs]
             if hasattr(self.lhs.output_field, 'get_path_info'):
                 self.rhs = [self.lhs.output_field.get_path_info()[-1].target_fields[-1].get_prep_value(rhs) for rhs in

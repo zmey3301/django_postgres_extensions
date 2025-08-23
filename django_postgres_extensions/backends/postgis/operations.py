f8a9ee040d42f8fac5dc444bfa54ee9ac2dce1c2
SoulString <mike@soulstring.io>
2025-08-23 12:39:38 +0400

    fix: properly configure spatial database operations

--- django_postgres_extensions/backends/postgis/operations.py
+++ django_postgres_extensions/backends/postgis/operations.py
@@ -1,4 +1,4 @@
-from django.contrib.gis.db.backends.postgis.operations import DatabaseOperations as BaseDatabaseOperations
+from django.contrib.gis.db.backends.postgis.operations import PostGISOperations
 
-class DatabaseOperations(BaseDatabaseOperations):
+class DatabaseOperations(PostGISOperations):
     compiler_module = "django_postgres_extensions.models.sql.compiler"
